17f07b7b8f8868a545bcb455078d6274d2884b5f
Peziim <eduardoorx@gmail.com>
2024-07-16 14:13:04 -0300

    feat: created StringPerfomance

---
+++ src/maratonajavadevdojo/String/test/StringPerfomanceTest.java
@@ -0,0 +1,36 @@
+package maratonajavadevdojo.String.test;
+
+public class StringPerfomanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(100000);
+        long fim = System.currentTimeMillis();
+        System.out.println("tempo: " + (fim - inicio) + "ms");
+
+
+        long inicio1 = System.currentTimeMillis();
+        concatStringBuffer(100000);
+        long fim1 = System.currentTimeMillis();
+        System.out.println("tempo do Buffer: " + (fim1 - inicio1) + "ms");
+
+
+
+
+    }
+
+    private static void concatString(int tamanho) {
+        String texto = "";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+        private static void concatStringBuffer(int size){
+            StringBuilder sb = new StringBuilder(size);
+            for (int i = 0; i < size; i++) {
+                sb.append(i);
+            }
+        }
+    }
+
+
+
