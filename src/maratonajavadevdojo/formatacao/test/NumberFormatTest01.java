5e5a4d487121022b3537e7ae8920a25761764323
Peziim <eduardoorx@gmail.com>
2024-07-17 11:19:44 -0300

    feat: created NumberFormatTest01

--- src/maratonajavadevdojo/formatacao/test/NumberFormatTest01.java
+++ src/maratonajavadevdojo/formatacao/test/NumberFormatTest01.java
@@ -8,11 +8,13 @@ public class NumberFormatTest01 {
         Locale localePT = new Locale("pt", "BR");
         Locale localeJap = Locale.JAPAN;
         Locale localeItaly = Locale.ITALY;
+
         NumberFormat [] numberFormat = new NumberFormat[4];
         numberFormat[0] = NumberFormat.getInstance(Locale.CHINA);
         numberFormat[1] = NumberFormat.getInstance(localePT);
         numberFormat[2] = NumberFormat.getInstance(localeItaly);
         numberFormat[3] = NumberFormat.getInstance(localeJap);
+
         double valor = 10_000.2130;
 
         for(NumberFormat currency: numberFormat){
