1f6033a45fee0fbb69ad9a1464ffe00472258eab
Peziim <eduardoorx@gmail.com>
2024-07-22 13:52:20 -0300

    feat: created InstantTest

---
+++ src/maratonajavadevdojo/datas/test/InstantTest01.java
@@ -0,0 +1,13 @@
+package maratonajavadevdojo.datas.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getEpochSecond());
+    }
+}
