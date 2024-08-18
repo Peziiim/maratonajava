3d11a6c75e4a47593d1e5d5e680d34d0413ebfab
Peziim <eduardoorx@gmail.com>
2024-08-18 16:16:40 -0300

    feat: created SubstringTest02

---
+++ src/maratonajavadevdojo/String/test/SubstringTest02.java
@@ -0,0 +1,33 @@
+package maratonajavadevdojo.String.test;
+
+import java.util.SplittableRandom;
+
+public class SubstringTest02 {
+    public static void main(String[] args) {
+        String s = "bnee";
+        String myFinalAnswerGuys = "";
+        String[] palindrome = new String[s.length()];
+        int length = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length() + 1; j++) {
+                String temp = s.substring(i, j);
+                String reverse = new StringBuilder(temp).reverse().toString();
+
+                if (temp.equals(reverse)) {
+                    palindrome[i] = reverse;
+                }
+
+            }
+        }
+
+        for(String dick: palindrome){
+            if (dick.length() > length){
+                length = dick.length();
+                myFinalAnswerGuys = dick;
+
+            }
+        }
+            return myFinalAnswerGuys;
+    }
+}
